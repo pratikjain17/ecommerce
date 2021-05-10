6653b15d7d21b016d13ea723fdbce7b635b6315c
kinnari <patyalkinni@gmail.com>
2021-05-10 08:21:58 -0700

    add a products

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -3,7 +3,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $database = "synccommerce";
+    $database = "syncommerce";
 
     $conn = mysqli_connect($servername,$username,$password,$database);
 ?>
